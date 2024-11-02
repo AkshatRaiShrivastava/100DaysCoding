3e13b4237fa3fa1ca7a34a08a26a198f52c8bf59
AkshatRaiShrivastava <shrivastavak2005@gmail.com>
2024-11-02 19:12:03 +0530

    day 12

--- searchInRotatedAndSortedArray.java
+++ searchInRotatedAndSortedArray.java
@@ -17,17 +17,27 @@ public class searchInRotatedAndSortedArray {
             if (arr[mid] == target) {
                 return mid;
             }
-            if (arr[mid] < target) {
-                low = mid+1;
+            if (arr[low] <= arr[mid]) {
+                if (target >= arr[low] && target<arr[mid]) {
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
             }
             else{
-                high = mid-1;
+                if (target > arr[mid] && target <= arr[high]) {
+                    low = mid + 1;
+                }
+                else{
+                    high = mid -1;
+                }
             }
         }
         return -1;
     }
     public static void main(String[] args) {
-        int[] arr = {4,5,6,1,2};
+        int[] arr = {6,1,2,3,4,5};
         int target = 6;
         System.out.println(findElementBin(arr,target));
     }
