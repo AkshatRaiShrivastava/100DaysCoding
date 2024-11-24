5a7c891d4479de0d3138c585a8ed23515ddb9e28
AkshatRaiShrivastava <shrivastavak2005@gmail.com>
2024-11-24 22:55:42 +0530

    day 21

---
+++ reverseWords.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class reverseWords {
+    public static String reverse(String str) {
+        int n = str.length();
+        int count = 0;
+        String res = "";
+        Stack<String> words = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            if (str.charAt(i) == '.') {
+                words.push(res);
+                count++;
+                res = "";
+            } else {
+                res = res + str.charAt(i);
+            }
+        }
+        words.push(res);
+        // giving output string
+        String result = "";
+        for (int i = 0; i <= count; i++) {
+            if (i == count) {
+                result = result + words.pop();
+            } else {
+                result = result + words.pop() + '.';
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String str = "i.like.coding.very.much";
+        System.out.println(reverse(str));
+    }
+}
